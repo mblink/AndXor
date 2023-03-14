6a86d67e1ee258ea24a4cbc404b781e12a6fab68
Matt Dziuban <mrdziuban@gmail.com>
2023-03-14 14:35:28 -0400

    Add script for working with both versions.

---
+++ both.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+operation="$1"; shift
+
+function sbtSequential() {
+  bash -c "cd scala-2 && sbt $1"
+  bash -c "cd scala-3 && sbt $1"
+}
+
+function sbtParallel() {
+  bash -c "cd scala-2 && sbt $1" &
+  bash -c "cd scala-3 && sbt $1" &
+  wait
+}
+
+function clean() {
+  sbtParallel clean
+}
+
+function compile() {
+  sbtParallel compile
+}
+
+function test() {
+  sbtParallel test
+}
+
+function generate() {
+  sbtParallel generate/run
+}
+
+function publish() {
+  sbtSequential publish
+}
+
+function gitRelease() {
+  sbtSequential gitRelease
+}
+
+$operation
