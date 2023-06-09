b67bc3c4032f28ac7ab91246c0e0938dc902f92a
Matt Dziuban <mrdziuban@gmail.com>
2023-06-09 17:09:27 -0400

    Remove unnecessary dummy implicits from constructors.

--- scala-3/generate/src/main/scala/andxor/template/AndXorNConstructors.scala
+++ scala-3/generate/src/main/scala/andxor/template/AndXorNConstructors.scala
@@ -3,8 +3,6 @@ package andxor.template
 object AndXorNConstructors {
   def apply(tpeLists: List[(List[String], Int)]) =
     s"""
-import andxor.types._
-
 trait AndXorNConstructors { ctors =>
   def nest[A[_[_]]]: AndXor._1Nested[A] = axoN[A]
 
@@ -13,10 +11,7 @@ ${tpeLists.map { case (tpes, i) =>
     if (tpes.tail.isEmpty) s"axo[${tpes.head}]"
     else s"new AndXor$i[${tpes.mkString(", ")}] {}"
 
-  s"""
-  @inline final def apply[${tpes.mkString(", ")}](using @annotation.unused d: Dummy$i): AndXor$i[${tpes.mkString(", ")}] =
-    $axo
-"""
+  s"  @inline final def apply[${tpes.mkString(", ")}]: AndXor$i[${tpes.mkString(", ")}] = $axo"
 }.mkString("\n")}
 }
 """
